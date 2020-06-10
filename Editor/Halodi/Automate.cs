0bb728482c6ccaf81289bb2614f78c7f885f2813
Felix Herbst <herbst@prefrontalcortex.de>
2020-06-10 13:46:19 +0200

    testing automation

---
+++ Editor/Halodi/Automate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Halodi.PackageRegistry;
+using UnityEngine;
+
+public class Automate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [UnityEditor.MenuItem("pfc/Automate")]
+    static void Aut()
+    {
+        var controller = new RegistryManagerController();
+        var reg = new ScopedRegistry();
+
+        reg.name = "needle-github"; 
+        reg.url = "https://npm.pkg.github.com/@needle-tools";
+        reg.scopes = new string[] { "com.needle" };
+
+        UpdateCredential(controller, reg);
+
+        // URL to generate tokens
+        // https://github.com/settings/tokens
+
+        // Felix' token - read:packages
+        var githubToken = "da48fccfde6413ccd9ddfb7377a9a5df865eb7cc";
+
+        reg.token = githubToken;
+        
+        controller.Save(reg);
+    }
+
+    private static void UpdateCredential(RegistryManagerController controller, ScopedRegistry registry)
+    {
+        if (controller.credentialManager.HasRegistry(registry.url))
+        {
+            NPMCredential cred = controller.credentialManager.GetCredential(registry.url);
+            registry.auth = cred.alwaysAuth;
+            registry.token = cred.token;
+        }
+    }
+}
